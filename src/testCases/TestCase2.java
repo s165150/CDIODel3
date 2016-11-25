5e4ec8b9f5e4ed0cf8662ff429b2c1b7f392d0a5
s165150 <s165150@student.dtu.dk>
2016-11-25 20:13:51 +0100

    New TestCase2

--- src/testCases/TestCase2.java
+++ src/testCases/TestCase2.java
@@ -1,14 +1,50 @@
+/**
+ * @author Morten N
+ */
+
 package testCases;
 
-import static org.junit.Assert.*;
 
+import java.awt.Color;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import game.*;
+import org.junit.*;
 
 public class TestCase2 {
+	
+	//Define attributes for test
+	private Player testPlayer1;
+	private MUI mui;
+	private GameBoard game = new GameBoard();
+	
+	//Precondition
+	@Before
+	public void setUp(){
+		//Create playerArray, testPlayer1 and adding him to the array and set field to not owned
+		testPlayer1 = new Player ("testPlayer1", 0, 1, 0);
+		testPlayer1.getAccount().setBalance(2000);
+		game.getPlayerlistArray().add(testPlayer1);
+		testPlayer1.setPosition(21);
 
+	}
+	//Test
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMove(){
+		
+		int expectedPosition = testPlayer1.setPosition(1);
+		int actualPosition = testPlayer1.getPosition()+2;
+		Assert.assertEquals(expectedPosition, actualPosition);
+		
+	}
+	
+	//Postcondition
+	@After
+	public void after() throws Exception{
+
+
+	
 	}
 
 }
