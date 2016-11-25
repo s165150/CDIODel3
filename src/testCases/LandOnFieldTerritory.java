875e992a5352ef4ae53839b58ca1ed4a2d076e79
s165150 <s165150@student.dtu.dk>
2016-11-25 18:14:13 +0100

    LandOnFIeld

--- src/testCases/LandOnFieldTerritory.java
+++ src/testCases/LandOnFieldTerritory.java
@@ -7,32 +7,32 @@ import java.awt.Color;
 import game.*;
 
 import org.junit.*;
-public class LandOnFieldRefuge {
+public class LandOnFieldTerritory {
 	private GameBoard game = new GameBoard();
 	private Player player;
 	private MUI mui;
 	private Shaker shake;
-	private Field refuge200;
+	private Field territory200;
 
-	private Field refuge0;
+	private Field territory0;
 
-	private Field refugeNeg200;
-
+	private Field territoryNeg200;
+	
 	@Before
-
 	public void setUp() throws Exception {
 
-	this.player = new Player("Anders And", 1, 1, 0);
-	player.getAccount().setBalance(1000);
-	game.getPlayerlistArray().add(player);
-	this.refuge200 = new Refuge("1. Helle", "Helle +200", null, null, null, 200);
-	game.getBoardFieldsArray().add(refuge200);
-	this.refuge0 = new Refuge("2. Helle", "Helle +200", null, null, null, 0);
-	game.getBoardFieldsArray().add(refuge0);
-	this.refugeNeg200 = new Refuge("3. Helle", "Helle +200", null, null, null, -200);
-	game.getBoardFieldsArray().add(refugeNeg200);
-	}
-
+		this.player = new Player("Anders And", 1, 1, 0);
+		player.getAccount().setBalance(1000);
+		game.getPlayerlistArray().add(player);
+		this.territory200 = new Territory("1. Helle", "Helle +200", null, null, null, 10, 1000, 100);
+		game.getBoardFieldsArray().add(territory200);
+		this.territory0 = new Territory("2. Helle", "Helle +200", null, null, null, 10, 1000, 100);
+		game.getBoardFieldsArray().add(territory0);
+		this.territoryNeg200 = new Territory("3. Helle", "Helle +200", null, null, null, 10, 1000, -200);
+		game.getBoardFieldsArray().add(territoryNeg200);
+		}
+	
+	
 	@After
 
 	public void tearDown() throws Exception {
@@ -41,7 +41,7 @@ public class LandOnFieldRefuge {
 
 	//The fields are unaltered
 
-	}
+}
 
 	@Test
 
@@ -49,40 +49,40 @@ public class LandOnFieldRefuge {
 
 	Assert.assertNotNull(this.player);
 
-	Assert.assertNotNull(this.refuge200);
+	Assert.assertNotNull(this.territory200);
 
-	Assert.assertNotNull(this.refuge0);
+	Assert.assertNotNull(this.territory0);
 
-	Assert.assertNotNull(this.refugeNeg200);
+	Assert.assertNotNull(this.territory200);
 
-	Assert.assertTrue(this.refuge200 instanceof Refuge);
+	Assert.assertTrue(this.territory200 instanceof Territory);
 
-	Assert.assertTrue(this.refuge0 instanceof Refuge);
+	Assert.assertTrue(this.territory0 instanceof Territory);
 
-	Assert.assertTrue(this.refugeNeg200 instanceof Refuge);
+	Assert.assertTrue(this.territoryNeg200 instanceof Territory);
 
 	}
-
+	
 	@Test
 
 	public void testLandOnField200() {
 
-	int expected = 1000;
+		int expected = 1000;
 
-	int actual = this.player.getAccount().getBalance();
+		int actual = this.player.getAccount().getBalance();
 
-	Assert.assertEquals(expected, actual);
+		Assert.assertEquals(expected, actual);
 
-	//Perform the action to be tested
+		//Perform the action to be tested
 
-	this.refuge200.landOnField(game, 0, 0, mui, shake);
+		this.territory200.landOnField(game, 0, 0, mui, shake);
 
-	expected = 1000 + 200;
+		expected = 1000 + 200;
 
-	actual = this.player.getAccount().getBalance();
+		actual = this.player.getAccount().getBalance();
 
-	Assert.assertEquals(expected, actual);
-
+		Assert.assertEquals(expected, actual);
+	
 	}
 
 	@Test
@@ -97,9 +97,9 @@ public class LandOnFieldRefuge {
 
 	//Perform the action to be tested
 
-	this.refuge200.landOnField(game, 0, 0, mui, shake);
+	this.territory200.landOnField(game, 0, 0, mui, shake);
 
-	this.refuge200.landOnField(game, 0, 0, mui, shake);
+	this.territory200.landOnField(game, 0, 0, mui, shake);
 
 	expected = 1000 + 200 + 200;
 
@@ -120,7 +120,7 @@ public class LandOnFieldRefuge {
 	Assert.assertEquals(expected, actual);
 	//Perform the action to be tested
 
-	this.refuge0.landOnField(game, 0, 0, mui, shake);
+	this.territory0.landOnField(game, 0, 0, mui, shake);
 
 	expected = 1000;
 
@@ -142,9 +142,9 @@ public class LandOnFieldRefuge {
 
 	//Perform the action to be tested
 
-	this.refuge0.landOnField(game, 0, 0, mui, shake);
+	this.territory0.landOnField(game, 0, 0, mui, shake);
 
-	this.refuge0.landOnField(game, 0, 0, mui, shake);
+	this.territory0.landOnField(game, 0, 0, mui, shake);
 
 	expected = 1000;
 
@@ -166,7 +166,7 @@ public class LandOnFieldRefuge {
 
 	//Perform the action to be tested
 
-	this.refugeNeg200.landOnField(game, 0, 0, mui, shake);
+	this.territoryNeg200.landOnField(game, 0, 0, mui, shake);
 
 	//It is not possible to deposit a negative amount
 
@@ -190,9 +190,9 @@ public class LandOnFieldRefuge {
 
 	//Perform the action to be tested
 
-	this.refugeNeg200.landOnField(game, 0, 0, mui, shake);
+	this.territoryNeg200.landOnField(game, 0, 0, mui, shake);
 
-	this.refugeNeg200.landOnField(game, 0, 0, mui, shake);
+	this.territoryNeg200.landOnField(game, 0, 0, mui, shake);
 
 	//It is still not possible to deposit a negative amount
 
