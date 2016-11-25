c1e5fd9794005de1083e3ffd4edc12f68df7e6ec
s165150 <s165150@student.dtu.dk>
2016-11-25 08:33:13 +0100

    First draft on test and diagrams

--- src/testCases/TestCase1.java
+++ src/testCases/TestCase1.java
@@ -1,14 +1,44 @@
+/**
+ * @author Morten N
+ */
+
 package testCases;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import game.*;
 
 public class TestCase1 {
-
+	
+	//Define attributes for test
+	Game game = new Game();
+	private Player testPlayer1;
+	private Player testPlayer2;
+	
+	
+	
+	//Precondition - Test case 1.1
+	@Before
+	public void setUp() throws Exception {
+		testPlayer1 = new Player ("testPlayer1", 1, 1, 0);
+		
+		
+	}
+	
+	//Test - Test case 1.1
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	}
+	
+	//Postcondation Test case 1.1
+	@After
+	public void after(){
+		
+
 	}
 
 }
